793517948781f1f883350a3deba355781fa1bf8c
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-06-10 00:19:48 +0530

    backend : added from Mockebee

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import { makeServer } from "./server";
+
+// Call make Server
+makeServer();
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+     
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
